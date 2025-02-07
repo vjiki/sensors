68d885812f7dd0b440e9ca648b186cbbb1c92fd9
vjiki <nezzabudka5@gmail.com>
2025-02-07 14:57:19 +0300

    monitoring: warehouse client

--- monitoring/build.gradle.kts
+++ monitoring/build.gradle.kts
@@ -10,6 +10,11 @@ repositories {
 }
 
 dependencies {
+    implementation("io.projectreactor.kafka:reactor-kafka:1.3.23")
+//    implementation("io.projectreactor:reactor-core:3.7.2")
+//    implementation("ch.qos.logback:logback-classic:1.5.16")
+
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
--- warehouse/build.gradle.kts
+++ warehouse/build.gradle.kts
@@ -10,6 +10,8 @@ repositories {
 }
 
 dependencies {
+    implementation("io.projectreactor.kafka:reactor-kafka:1.3.23")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
